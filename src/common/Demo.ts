e9b349aae2a928bb539127b8b4541689630ce953
Joe Eli McIlvain <joe.eli.mac@gmail.com>
2022-07-08 13:12:24 -0700

    Some minor refactoring and tidying up of Demo1.

--- src/common/Demo.ts
+++ src/common/Demo.ts
@@ -34,6 +34,17 @@ export default abstract class Demo {
     fn(commandEncoder)
     this.device.queue.submit([commandEncoder.finish()])
   }
+
+  // Some browsers don't yet support the `layout: "auto"`, so for them
+  // we introduce this hack that pretends that `undefined` is that string
+  // at the TypeScript level, so that the browser can fill it in as the default,
+  // despite the types no longer allowing for it to be undefined.
+  //
+  // Eventually this hack will stop working and we'll just replace this
+  // with actually returning the string "auto" and it will be better.
+  autoLayout() {
+    return undefined as unknown as GPUAutoLayoutMode
+  }
 }
 
 export function runDemo<T extends Demo>(type: { new (props: DemoProps): T }) {
