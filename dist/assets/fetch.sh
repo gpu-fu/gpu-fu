354aa3d48c0dffe6ec5d1a8d4ba81add4029a129
Joe Eli McIlvain <joe.eli.mac@gmail.com>
2022-07-10 13:27:10 -0700

    Add missing file `dist/assets/fetch.sh`.

---
+++ dist/assets/fetch.sh
@@ -0,0 +1,2 @@
+# Photo Credit to Hillebrand Steve, USFWS
+ls fireweed.jpg || wget https://pixnio.com/free-images/fauna-animals/deers/sitka-black-tailed-deer-with-fireweed-850x1275.jpg -O fireweed.jpg
